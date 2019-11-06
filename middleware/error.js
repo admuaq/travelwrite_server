68488d7fde3b011b1d3c628815ecf1bf5a2875d0
admuaq <admuaq@gmail.com>
2019-11-06 12:41:04 +0000

    Presetup to handle logging and system fails. Return to complete

--- middleware/error.js
+++ middleware/error.js
@@ -1,5 +1,6 @@
 module.exports = function (err, req, res, next) {
-  // Log the exception
+  // Log the exception on database server failure
+  // Return to complete
   console.log(err)
   res.status(500).send('Something has failed')
 }
